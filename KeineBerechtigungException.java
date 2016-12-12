b99967b59389c9f1c06244b787cc66685cbadc7b
Khaled Halabieh <khaled-h@hotmail.it>
2016-12-12 10:12:06 +0100

    Create KeineBerechtigungException.java

---
+++ KeineBerechtigungException.java
@@ -0,0 +1,14 @@
+package hausaufgabe9;
+
+public class KeineBerechtigungException extends Exception{
+
+
+	public KeineBerechtigungException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+
+}
